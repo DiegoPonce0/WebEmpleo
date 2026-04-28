2272cbf026f4fb1f8a42cde3f0636eeb772c4523
Diego Ponce <diego_lpz29@hotmail.com>
2026-04-28 04:57:23 -0700

    fix: update 404 error message for clarity

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -2,7 +2,7 @@ export default function NotFound() {
     return (
         <main>
             <h1>404 - Page not found</h1>
-            <p>Page that you are looking for does not exist anymore</p>
+            <p>The page that you are looking for does not exist</p>
         </main>
     )
 }
