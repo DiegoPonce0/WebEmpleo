81f72add9ec7444bfc8904ff945273477d57541e
Diego Ponce <diego_lpz29@hotmail.com>
2026-05-04 01:09:27 -0700

    fix: update database connection to use DATABASE_URL and SSL configuration

--- backend/models/postgresql/job.js
+++ backend/models/postgresql/job.js
@@ -4,14 +4,21 @@ import pkg from 'pg';
 import { formatJob } from '../../utils/formatJob.js';
 const { Pool } = pkg;
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
 
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export class JobModel {
     static async getAll({ location, text, level, technology, limit = DEFAULTS.LIMIT_PAGINATION, offset = DEFAULTS.LIMIT_OFFSET }) {
